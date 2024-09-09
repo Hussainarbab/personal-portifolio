566bc53ab2a3c572abf71265228a0e71c184deac
Hussainarbab <arbabhussan63@gmail.com>
2024-09-09 15:52:33 +0500

    commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const ok =  document.querySelector("body");
+const okk =  document.querySelector("body");
+
+
+function dark(){
+ok.style.backgroundColor= "black"
+ok.style.color= "white"
+}
+
+
+
+function light(){
+okk.style.backgroundColor= "white"
+okk.style.color= "black"
+}
+
+
